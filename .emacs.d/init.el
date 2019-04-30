64f66f1172a6f5067c3795e265321b7a955a59b5
mgmorey <mgmorey@gmail.com>
2019-04-29 17:51:32 -0700

    Use program "gls" for dired function in GNU Emacs.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,34 +1,36 @@
-(require 'package)
+(when (> emacs-major-version 22)
+  (require 'package)
 
-(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
-                    (not (gnutls-available-p))))
-       (proto (if no-ssl "http" "https")))
-  ;; Comment/uncomment these two lines to enable/disable MELPA and MELPA Stable as desired
-  (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
-  ;;(add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
-  (when (< emacs-major-version 24)
-    ;; For important compatibility libraries like cl-lib
-    (add-to-list 'package-archives '("gnu" . (concat proto "://elpa.gnu.org/packages/")))))
+  (let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
+		      (not (gnutls-available-p))))
+	 (proto (if no-ssl "http" "https")))
+    ;; Comment/uncomment these two lines to enable/disable MELPA and MELPA Stable as desired
+    (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
+    ;;(add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
+    (when (< emacs-major-version 24)
+      ;; For important compatibility libraries like cl-lib
+      (add-to-list 'package-archives '("gnu" . (concat proto "://elpa.gnu.org/packages/")))))
 
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
-(package-initialize)
+  ;; Added by Package.el.  This must come before configurations of
+  ;; installed packages.  Don't delete this line.  If you don't want it,
+  ;; just comment it out by adding a semicolon to the start of the line.
+  ;; You may delete these explanatory comments.
+  (package-initialize)
 
-;; (setq mac-command-modifier 'meta)
-;; (setq mac-option-modifier 'control)
+  ;; (setq mac-command-modifier 'meta)
+  ;; (setq mac-option-modifier 'control)
 
-;; (setq insert-directory-program "gls" dired-use-ls-dired t)
+  ;; (setq insert-directory-program "gls" dired-use-ls-dired t)
 
-;; (setq inferior-lisp-program "sbcl")
-;; (setq slime-contribs '(slime-fancy))
+  ;; (setq inferior-lisp-program "sbcl")
+  ;; (setq slime-contribs '(slime-fancy))
 
-;; (require 'helm-config)
-;; (require 'slime-autoloads)
+  ;; (require 'helm-config)
+  ;; (require 'slime-autoloads)
 
-(global-set-key (kbd "C-x g") 'magit-status)
-;; (helm-mode 1)
+  (global-set-key (kbd "C-x g") 'magit-status)
+  ;; (helm-mode 1)
+  )
 
 (add-to-list 'auto-mode-alist '("Pipfile.lock\\'" . javascript-mode))
 (add-to-list 'auto-mode-alist '("Pipfile\\'" . conf-mode))
