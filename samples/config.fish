b72bd2d915ee6468a25820ecbae561e458efee4b
Michael G. Morey <mgmorey@gmail.com>
2020-05-09 19:21:38 -0700

    Invoke script ~/bin/set-parameters from within config.fish.

--- samples/config.fish
+++ samples/config.fish
@@ -1,3 +1,4 @@
+# config.fish
 set -x EDITOR emacs
 set -x FLASK_ENV development
-set -x PATH ~/.pyenv/bin:~/.local/bin:~/.cargo/bin:~/bin:/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin
+eval (~/bin/set-parameters)
