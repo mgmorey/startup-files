446302943dfffe49ef3feee5a495c72234377ae2
Michael G. Morey <mgmorey@gmail.com>
2021-03-13 08:32:22 -0600

    Add commented code to bootstrap use-package and install ace-window.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -75,6 +75,21 @@
 ;;     (define-key input-decode-map "^[[1;5C" [(control right)])
 ;;     (define-key input-decode-map "^[[1;5D" [(control left)])))
 
+;; ;; Bootstrap use-package
+;; (unless (package-installed-p 'use-package)
+;;   (package-refresh-contents)
+;;   (package-install 'use-package))
+
+;; (use-package ace-window
+;;   :ensure t
+;;   :init
+;;   (progn
+;;     (global-set-key [remap other-window] 'ace-window)
+;;     (custom-set-faces
+;;       '(aw-leading-char-face
+;;          ((t (:inherit ace-jump-face-foreground :height 3.0)))))
+;;     ))
+
 ;; Default to indenting with spaces (rather than tabs)
 (setq-default indent-tabs-mode nil)
 
