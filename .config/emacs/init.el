93ad4d9f77b94600ce88462c1aa2644ae2bf95ea
Michael G. Morey <mgmorey@gmail.com>
2022-02-25 21:42:09 -0600

    Add Emacs Lisp statements to configure package paredit to init.el.

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -103,6 +103,19 @@
   ;; (modus-themes-load-operandi) ;; OR (modus-themes-load-vivendi)
   ;; (define-key global-map (kbd "<f5>") #'modus-themes-toggle)
 
+  ;; ;; Configure paredit
+  ;; (require 'paredit)
+  ;; (add-hook 'emacs-lisp-mode-hook 'enable-paredit-mode)
+  ;; (add-hook 'eval-expression-minibuffer-setup-hook 'enable-paredit-mode)
+  ;; (add-hook 'ielm-mode-hook 'enable-paredit-mode)
+  ;; (add-hook 'lisp-interaction-mode-hook 'enable-paredit-mode)
+  ;; (add-hook 'lisp-mode-hook 'enable-paredit-mode)
+  ;; (add-hook 'slime-repl-mode-hook 'enable-paredit-mode)
+  ;; (defun override-slime-del-key ()
+  ;;   (define-key slime-repl-mode-map
+  ;;     (read-kbd-macro paredit-backward-delete-key) nil))
+  ;; (add-hook 'slime-repl-mode-hook 'override-slime-del-key)
+
   ;; ;; Configure slime
   ;; (require 'slime-autoloads)
   ;; (setq inferior-lisp-program "sbcl")
