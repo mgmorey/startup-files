7c5107dc48cd66178e884986ae403d82c22f664f
Michael G. Morey <mgmorey@gmail.com>
2022-03-08 07:09:42 -0600

    Reindent Emacs Lisp statements for adding package archives to list.

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -12,11 +12,11 @@
     ;; Comment/uncomment the next two statements to enable/disable
     ;; MELPA and MELPA Stable as desired
     ;; (add-to-list 'package-archives
-    ;;   (cons "melpa"
-    ;;     (concat proto "://melpa.org/packages/")) t)
+    ;;              (cons "melpa"
+    ;;                    (concat proto "://melpa.org/packages/")) t)
     (add-to-list 'package-archives
-      (cons "melpa-stable"
-        (concat proto "://stable.melpa.org/packages/")) t)
+                 (cons "melpa-stable"
+                       (concat proto "://stable.melpa.org/packages/")) t)
     (when (< emacs-major-version 24)
       ;; For important compatibility libraries like cl-lib
       (add-to-list 'package-archives
